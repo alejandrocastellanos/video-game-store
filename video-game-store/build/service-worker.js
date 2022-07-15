9d8a103f8d7fff49526e3604a807908e12eab9c9
Alejandro Castellanos <alejandro.castellanos@grupor5.com>
2022-07-15 11:48:43 -0500

    feat: build

--- video-game-store/build/service-worker.js
+++ video-game-store/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/video-game-store/precache-manifest.efbec9ae8a985322dc898ff7bf4c4606.js"
+  "/precache-manifest.4d1f62bee3b6677b6da1bb24941ddee0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/video-game-store/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
