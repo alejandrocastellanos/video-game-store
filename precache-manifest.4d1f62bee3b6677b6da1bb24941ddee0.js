8bc691418027e708e0bf7349e67ba80505ed91cc
Alejandro Castellanos <alejandro.castellanos@grupor5.com>
2022-07-15 11:56:35 -0500

    Updates

---
+++ precache-manifest.4d1f62bee3b6677b6da1bb24941ddee0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58d322bf10bde3a09e10",
+    "url": "/static/css/main.e4e9e064.chunk.css"
+  },
+  {
+    "revision": "58d322bf10bde3a09e10",
+    "url": "/static/js/main.58d322bf.chunk.js"
+  },
+  {
+    "revision": "38c8e9c1748e4ce77544",
+    "url": "/static/js/1.38c8e9c1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f004a6dc89aa6bde1ef9e3bd442993d",
+    "url": "/index.html"
+  }
+];
